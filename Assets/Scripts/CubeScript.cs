74392acc7d2db94e318ee1baf460cbcdaf27d6e0
Ernesto C. Janebro <ernesto.camurca@gmail.com>
2014-09-23 01:40:40 -0300

    Commit inicial do projeto.

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	public Color colorPlayer1 = Color.yellow;
+	public Color colorPlayer2 = Color.black;
+	public int turn;
+	public GameManagerScript gameManagerScript;
+
+	// Use this for initialization
+	void Start () {
+		turn = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void onMouseUpAsButton() {
+		if (turn == 0) {
+			if (gameManagerScript.player == 1) {
+				this.renderer.material.color = colorPlayer1; 
+			} else {
+				this.renderer.material.color = colorPlayer2;
+			}
+
+			gameManagerScript.nextTurn();
+		}
+	}
+}
---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+
+	//Starting with player 1
+	public int player = 1;
+	public CubeScript[] cubes;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void nextTurn(){
+		if (player == 1) {
+			player = 2;
+		} else {
+			player = 1;
+		}
+	}
+}
